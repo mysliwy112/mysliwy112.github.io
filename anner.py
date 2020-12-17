fa15ee3d0f28efa56d4a7b4b0a9b5429e423198c
mysliwy112 <112mysliwy112@wp.pl>
2020-12-17 14:14:00 +0100

    Updated index.json, and changed # to _

--- anner.py
+++ anner.py
@@ -8,9 +8,9 @@ anndrzemPath=r"E:\AidemMedia\ANN-decoder\bin\Release\Anndrzem.exe"
 inputPaths=[
 	[r"E:\AidemMedia\ORIG\Reksio i Skarb Piratów ORIG","RiSP",{"Dane":"","Intro":"","MainMenu":""}],
 	[r"E:\AidemMedia\ORIG\Reksio i Ufo","RiU",{"DANE":"","ReksioUfo":"","PRZYGODA":""}],
-	[r"E:\AidemMedia\ORIG\Reksio i Czarodzieje O","RiC",{"common":"#common","dane":"","game":"","Przygoda":"","intro":"#intro"}],
-	[r"E:\AidemMedia\ORIG\Reksio i Wehikuł Czasu ORIG","RiWC",{"common":"#common","Dane":"","Game":"","Przygoda":""}],
-	["D:\\","RiKN",{"common":"#common","dane":"","game":"","przygoda":""}]
+	[r"E:\AidemMedia\ORIG\Reksio i Czarodzieje O","RiC",{"common":"_common","dane":"","game":"","Przygoda":"","intro":"_intro"}],
+	[r"E:\AidemMedia\ORIG\Reksio i Wehikuł Czasu ORIG","RiWC",{"common":"_common","Dane":"","Game":"","Przygoda":""}],
+	["D:\\","RiKN",{"common":"_common","dane":"","game":"","przygoda":""}]
 ]
 outputPath=r"E:\AidemMedia\ANNbrowser\filesys"
 
@@ -18,10 +18,14 @@ characters={"reksio":"reksio","kret":"kretes"}
 
 dataPath=r"E:\AidemMedia\ANNbrowser\index.json"
 meta=[]
+
+addAnns=0;
+
+
 with open(dataPath,"r") as database:
 	try:
-		pass
-		#json.load(database)
+		if addAnns==1:
+			json.load(database)
 	except json.decoder.JSONDecodeError:
 		meta=[]
 try:		
@@ -36,10 +40,11 @@ try:
 				out=out[1:]
 			for file in filenames:
 				if ".ann" in file:
-					sub.run([anndrzemPath,os.path.join(dirpath,file),"-d="+os.path.join(outputPath,inP[1],out,''),"-j","--full"])
-					shutil.copyfile(os.path.join(dirpath,file),os.path.join(outputPath,inP[1],out,file.replace(".ann",''),file))
+					if addAnns==1:
+						sub.run([anndrzemPath,os.path.join(dirpath,file),"-d="+os.path.join(outputPath,inP[1],out,''),"-j","--full"])
+						shutil.copyfile(os.path.join(dirpath,file),os.path.join(outputPath,inP[1],out,file.replace(".ann",''),file))
 					info={}
-					info["path"]=os.path.join(inP[1],out)
+					info["path"]=os.path.join(inP[1],out).replace('\\','/')
 					info["characters"]=[characters[char]  for char in characters if char in file]
 					info["name"]=file.replace(".ann",'')
 					meta.append(info)
